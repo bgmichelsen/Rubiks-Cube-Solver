a12bba9876d84c9648c3f17b34907cb5e08c2c14
Brandon Michelsen <brandongmichelsen@gmail.com>
2020-10-07 21:29:45 -0700

    Added classes Matrix3D and Point3D

--- maths/matrices.py
+++ maths/matrices.py
@@ -1,5 +1,149 @@
-# TODO: Implement matrix class
+"""
+===============================================================
+                        Matrix class
+===============================================================
 
-class Matrix:
-    def __init__(self):
+Defines the Matrix3D class
+
+"""
+
+from maths.points import Point3D
+from numbers import Number
+
+
+class Matrix3D:
+    """
+    A class for working with 3x3x3 matrices
+    """
+
+    def __init__(self, *args):
+        """
+        :param args: Args for entering the data into the matrix
+                     must be numeric and must have 9 elements
+        """
+
+        self.data = None
+
+        if len(args) == 9:
+            # Args entered: 1, 2, 3, 4, 5, 6, 7, 8, 9
+            self.data = list(args)
+        elif len(args) == 3:
+            # Args entered: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+            self.data = [elem for sub in args for elem in sub]
+        else:
+            # Args entered: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            self.data = args[0]
+
+        if len(self.data) != 9:
+            raise ValueError(f"Matrix3D must have 9 elements, received {args}")
+
+        if any(elem is None for elem in self.data):
+            raise ValueError(f"Matrix3D does not accept None type")
+
+    def rows(self):
+        """
+        Get the rows in the matrix
+
+        :return: The rows in the matrix
+        """
+
+        yield self.data[0:3]
+        yield self.data[3:6]
+        yield self.data[6:9]
+
+    def cols(self):
+        """
+        Get the columns in the matrix
+
+        :return: The columns in the matrix
+        """
+
+        yield self.data[0:9:3]
+        yield self.data[1:9:3]
+        yield self.data[2:9:3]
+
+    def eigenvector(self):
+        """
+        Calculate the eigenvector of the matrix
+
+        :return:
+        """
+
         pass
+
+    def eigenval(self):
+        """
+        Calculate the eigenvalue of the matrix
+
+        :return:
+        """
+        pass
+
+    def det(self):
+        """
+        Find the determinant of the matrix
+
+        :return: The determinant
+        """
+
+        a = self.data[0]*(self.data[4]*self.data[8] - self.data[5]*self.data[7])
+        b = self.data[1]*(self.data[3]*self.data[8] - self.data[5]*self.data[6])
+        c = self.data[2]*(self.data[3]*self.data[7] - self.data[4]*self.data[6])
+
+        return a - b + c
+
+    def __add__(self, other):
+        """
+        Add operator
+
+        :param other: The other component to add
+        :return: A matrix of the sum
+        """
+
+        assert type(other) == Matrix3D
+
+        return Matrix3D(a + b for a, b in zip(self.vals, other.vals))
+
+    def __sub__(self, other):
+        """
+        Subtraction operator
+
+        :param other: The other component to subtract
+        :return: A matrix of the sum
+        """
+
+        assert type(other) == Matrix3D
+
+        return Matrix3D(a - b for a, b in zip(self.vals, other.vals))
+
+    def __mul__(self, other):
+        """
+        Multiplication operator
+
+        :param other: The other component to multiply
+        :return:
+        """
+
+        if isinstance(other, Point3D):
+            return Point3D(other.dot(Point3D(row)) for row in self.rows())
+        elif isinstance(other, Matrix3D):
+            return Matrix3D(Point3D(row).dot(Point3D(col)) for row in self.rows \
+                            for col in self.cols())
+        elif isinstance(other, Number):
+            return Matrix3D(a*other for a in self.data)
+        else:
+            raise ValueError(f"Matrix3D can only multiply the following types: "\
+                             "Point3D, Matrix3D, Number")
+
+    def __str__(self):
+        return "[{}, {}, {}\n"\
+                "{}, {}, {}\n"\
+                "{}, {}, {}]".format(*self.data)
+
+    def __repr__(self):
+        return "Matrix3D " + str(self)
+
+
+# Identity matrix
+IDENTITY_MATRIX = Matrix3D([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
--- maths/points.py
+++ maths/points.py
@@ -1,5 +1,193 @@
-# TODO: Create point class and functions
+"""
+============================================================
+                    Point Class
+============================================================
 
-class Point:
-    def __init__(self):
-        pass
+Defines the Point3D class
+
+"""
+
+from numbers import Number
+
+
+class Point3D:
+    """
+    A class for working with 3D points
+    """
+
+    def __init__(self, x, y=None, z=None):
+        """
+        :param x: Tuple of all the coords or just the x coord
+        :param y: y coord of point
+        :param z: z coord of point
+        """
+
+        self.x = None
+        self.y = None
+        self.z = None
+
+        if isinstance(x, (tuple, list)):
+            assert len(x) == 3
+
+            self.x = x[0]
+            self.y = x[1]
+            self.z = x[2]
+        else:
+            self.x = x
+            self.y = y
+            self.z = z
+
+        if any(elem is None for elem in x):
+            raise ValueError(f"Point does not accept None types")
+
+    def get_coords(self):
+        """
+        :return: The coordinates stored in the Point
+        """
+
+        return self.x, self.y, self.z
+
+    def dot(self, other):
+        """
+        Calcualte the dot product of the point and another point
+
+        :param other: The other point
+        :return: The dot product of the two points
+        """
+
+        assert type(other) == Point3D
+
+        return self.x*other.x + self.y*other.y + self.z*other.z
+
+    def cross(self, other):
+        """
+        Calcualte the cross product of the point and another point
+
+        :param other: The other point
+        :return: The cross product of the two points (a new point)
+        """
+
+        assert type(other) == Point3D
+
+        c_x = self.y*other.z - self.z*other.y
+        c_y = self.z*other.x - self.x*other.z
+        c_z = self.x*other.y - self.y*other.x
+
+        return Point3D(c_x, c_y, c_z)
+
+    def __add__(self, other):
+        """
+        Add operator
+
+        :param other: Other point to add
+        :return: The addition of the point and another point
+        """
+
+        c_x = None
+        c_y = None
+        c_z = None
+
+        if isinstance(other, Point3D):
+            c_x = self.x + other.x
+            c_y = self.y + other.y
+            c_z = self.z + other.z
+        elif isinstance(other, (tuple, list)):
+            assert len(other) == 3
+
+            c_x = self.x + other[0]
+            c_y = self.y + other[1]
+            c_z = self.z + other[2]
+        else:
+            raise ValueError(f"Point3D can only add the following types: " \
+                                 "Point3D, tuple, list")
+
+        return Point3D(c_x, c_y, c_z)
+
+    def __sub__(self, other):
+        """
+        Subtract operator
+
+        :param other: Other point to subtract
+        :return: The subtraction of the point and another point
+        """
+
+        c_x = None
+        c_y = None
+        c_z = None
+
+        if isinstance(other, Point3D):
+            c_x = self.x - other.x
+            c_y = self.y - other.y
+            c_z = self.z - other.z
+        elif isinstance(other, (tuple, list)):
+            assert len(other) == 3
+
+            c_x = self.x - other[0]
+            c_y = self.y - other[1]
+            c_z = self.z - other[2]
+        else:
+            raise ValueError(f"Point3D can only subtract the following types: "\
+                             "Point3D, tuple, list")
+
+        return Point3D(c_x, c_y, c_z)
+
+    def __mul__(self, other):
+        """
+        Multiplication operator
+
+        :param other: Other point to multiply
+        :return: The product of the two points
+        """
+
+        c_x = None
+        c_y = None
+        c_z = None
+
+        if isinstance(other, Point3D):
+            c_x = self.x*other.x
+            c_y = self.y*other.y
+            c_z = self.z*other.z
+        elif isinstance(other, (tuple, list)):
+            assert len(other) == 3
+
+            c_x = self.x*other[0]
+            c_y = self.y*other[1]
+            c_z = self.z*other[2]
+        elif isinstance(other, Number):
+            c_x = self.x*other
+            c_y = self.y*other
+            c_z = self.z*other
+        else:
+            raise ValueError(f"Point3D can only be multiplied by the following types: "\
+                             "Point3D, tuple, list, or Number")
+
+        return Point3D(c_x, c_y, c_z)
+
+    def __eq__(self, other):
+        """
+        Check if values are equal
+
+        :param other: The other object (must be tuple, list, or
+        :return: If the objects are equal
+        """
+
+        if isinstance(other, (tuple, list)):
+            assert len(other) == 3
+
+            return self.x == other[0] and self.y == other[1] and self.z == other[2]
+        return (isinstance(other, Point3D) and self.x == other.x \
+                and self.y == other.y and self.z == other.z)
+
+    def __ne__(self, other):
+        return self == other
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
+
+    def __str__(self):
+        return "({}, {}, {})".format(self.x, self.y, self.z)
+
+    def __repr__(self):
+        return "Point" + str(self)
